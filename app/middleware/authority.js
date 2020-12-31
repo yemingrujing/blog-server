30986d7cd18ca8fc6fbb248a61db56f12464afca
光伟 <yemingrujing@gmail.com>
2020-12-31 17:59:22 +0800

    登录鉴权

--- app/middleware/authority.js
+++ app/middleware/authority.js
@@ -5,11 +5,25 @@ const whiteList = ['/api/user/login', '/api/user/userInfo', '/api/user/logout', 
 module.exports = () => {
   return async function auth(ctx, next) {
     const url = ctx.originalUrl,
-      web = /^\/web\//;
+      web = /^\/web\//,
+      token = ctx.request.header.authorization;
     ctx.logger.info('url：' + url);
     if (whiteList.includes(url) || web.test(url)) {
       await next();
     } else if (ctx.session.username) {
+      if (!token) {
+        ctx.throw(401, '您需要先登陆以后才能操作');
+      }
+      ctx.logger.info('token：' + token);
+      ctx.logger.info('token：' + ctx.app.config.jwt.secret);
+      // 验证当前token
+      const decode = ctx.app.jwt.verify(token, ctx.app.config.jwt.secret);
+      if (!decode || !decode.userName) {
+        ctx.throw(401, '没有权限，请登录');
+      }
+      if (Date.now() - decode.expire > 0) {
+        ctx.throw(401, 'Token已过期');
+      }
       if (ctx.session.permission.includes(url)) {
         await next();
       } else {
